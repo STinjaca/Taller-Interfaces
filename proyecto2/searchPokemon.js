ccbfeb646bee724a33ddbec12c359eae4dfd3c23
Santi T <s.tinjaca@uniandes.edu.co>
2024-04-05 23:39:01 -0500

    inicio voz

---
+++ proyecto2/searchPokemon.js
@@ -0,0 +1,6 @@
+// Función para buscar información sobre el Pokémon
+function searchPokemon(pokemonName) {
+    // Simplemente alerta el nombre del Pokémon por ahora
+    alert("Buscando información sobre el Pokémon: " + pokemonName);
+    // Aquí puedes realizar una solicitud a una API de Pokémon para obtener datos sobre el Pokémon y mostrarlos en la página
+}
