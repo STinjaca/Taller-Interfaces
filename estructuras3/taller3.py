06f09fc4f25f7c7e4622a1c24a254f3ae408b9f3
Santi T <s.tinjaca@uniandes.edu.co>
2024-04-06 19:30:22 -0500

    hi

--- estructuras3/taller3.py
+++ estructuras3/taller3.py
@@ -9,20 +9,40 @@ revisado = {}
 def root():
     return {"Servicio": "Estructuras de datos"}
 
-@app.post('/api/v1/search')
+#Url que recibe un parametro tipo_tutela
+@app.get('/api/v1/search')
 def busqueda_tutela(tipo_tutela: str):
-    print (tipo_tutela)
-    '''    if tipo_tutela in revisado:
+    
+    #Verfica si ya se habia revisado este tipo de tutela
+    if tipo_tutela in revisado:
         return revisado[tipo_tutela]
 
-    result = {
-        
-    }
+    #Diccionrio decretos revisado
+    decretos = {}
+    #Lista de respuesta
+    result = []
 
+    #Recorrer cada tutela en el archivo
     for indice, tutela_info in enumerate(mook.tutela):
+        #comprobar si es el tipo de tutela
         if tipo_tutela.lower() in tutela_info["resumen"].lower():
+            #obtener el numero de decreto
+            decreto = tutela_info["decreto"] 
+            #Comprueba si el decreto ya se habia revisado
+            if decreto not in decretos:
+                #Ubicar el decreto en la lista
+                decretos[decreto] = len(result)
+                data = { "decreto": decreto,
+                        "tutelas":[]
+                }
+                result.append(data)
+            else:
+                #obtener datos de ese decreto
+                data = result[decretos[decreto]]
             
-            result["tutelas"].append(tutela_info)
+            #añadir tutela a lista del decreto
+            data["tutelas"].append(tutela_info)
 
-    revisado[tipo_tutela] = result'''
-    return "result"
+    #añadir para futuras consultas
+    revisado[tipo_tutela] = result
+    return result
